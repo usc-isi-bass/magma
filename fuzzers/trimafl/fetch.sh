c522d09f83a0034ffd9ebb01621474dfe01e643f
Wei-Cheng Wu <wwu@isi.edu>
2021-03-02 15:54:56 -0800

    Init for trimafl

---
+++ fuzzers/trimafl/fetch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env FUZZER: path to fuzzer work dir
+##
+
+git clone --no-checkout https://github.com/google/AFL.git "$FUZZER/repo"
+git -C "$FUZZER/repo" checkoutfab1ca5ed7e3552833a18fc2116d33a9241699bc 
+#wget -O "$FUZZER/repo/afl_driver.cpp" \
+#    "https://cs.chromium.org/codesearch/f/chromium/src/third_party/libFuzzer/src/afl/afl_driver.cpp"
+cp "$FUZZER/src/afl_driver.cpp" "$FUZZER/repo/afl_driver.cpp"
+
+patch -p1 -d "$FUZZER/repo" < "$FUZZER/afl.patch"
